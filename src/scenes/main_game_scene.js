52f7af91b1a86b91e0080bce39814bda4596bb20
Gustavo Henrique Santos Souza de Miranda <gustavohssmiranda@gmail.com>
2023-11-08 20:03:46 -0300

    Adicionado as sprites dos paddles do jogo

--- src/scenes/main_game_scene.js
+++ src/scenes/main_game_scene.js
@@ -22,14 +22,21 @@ class main_game_scene extends Phaser.Scene{
      * @description Essa função carrega para a memoria os recursos da cena.
      */
     preload(){
-        this.load.image('divider_black','../../assets/images/Divider_Black.png')
+        this.load.image('paddle_left_white','../../assets/images/Paddle_White.png');
+        this.load.image('paddle_right_white','../../assets/images/Paddle_White.png');
+        this.load.image('divider_black','../../assets/images/Divider_Black.png');
+        this.load.image('ball_white',   '../../assets/images/Ball_White.png');
+        
 
     }
         /**
      * @description Essa função cria os recursos usados na cena
      */
     create(){
+        this.add.sprite(45,390,'paddle_left_white');
         this.add.sprite(510,390, 'divider_black');
-
+        this.add.sprite(975,390, 'paddle_right_white');
+        this.add.sprite(390,390, 'ball_white');
+        
     }
 }
