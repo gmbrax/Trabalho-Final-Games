79ca6a0c9921b7bcb3de7ea1e052e1d249bb4147
Gustavo Henrique Santos Souza de Miranda <gustavohssmiranda@gmail.com>
2023-11-09 13:36:05 -0300

    Adicionado as sprites para o score e alterado as fontes de numero para serem maior.

--- src/scenes/main_game_scene.js
+++ src/scenes/main_game_scene.js
@@ -22,21 +22,26 @@ class main_game_scene extends Phaser.Scene{
      * @description Essa função carrega para a memoria os recursos da cena.
      */
     preload(){
-        this.load.image('paddle_left_white','../../assets/images/Paddle_White.png');
-        this.load.image('paddle_right_white','../../assets/images/Paddle_White.png');
+        this.load.image('paddle_white','../../assets/images/Paddle_White.png');
         this.load.image('divider_black','../../assets/images/Divider_Black.png');
         this.load.image('ball_white',   '../../assets/images/Ball_White.png');
-        
+        this.load.spritesheet("Number_Font_White","../../assets/images/Number_Font_White.png",{frameWidth:40,frameHeight:80,spacing:21});
 
     }
         /**
      * @description Essa função cria os recursos usados na cena
      */
     create(){
-        this.add.sprite(45,390,'paddle_left_white');
+        this.add.sprite(45,390,'paddle_white');
+        this.add.sprite(975,390, 'paddle_white');
         this.add.sprite(510,390, 'divider_black');
-        this.add.sprite(975,390, 'paddle_right_white');
         this.add.sprite(390,390, 'ball_white');
+        this.add.sprite(174,160,'Number_Font_White',9);
+        this.add.sprite(234,160,'Number_Font_White',9);
+        this.add.sprite(294,160,'Number_Font_White',9);
+        this.add.sprite(690,160,'Number_Font_White',9);
+        this.add.sprite(750,160,'Number_Font_White',9);
+        this.add.sprite(810,160,'Number_Font_White',9);
         
     }
 }
