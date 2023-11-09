b5647a21c9ebbfdc5815e58fbeb3f060e7f924ab
Gustavo Henrique Santos Souza de Miranda <gustavohssmiranda@gmail.com>
2023-11-09 16:51:27 -0300

    Adicionado no main.js configurações de escala e configuração de fisica do jogo.

--- main.js
+++ main.js
@@ -7,6 +7,62 @@
 /**
  * @author Gustavo Henrique S. S. Miranda
  * @class 
+ * @classdesc Essa classe contem os dados referentes a como é feito o escaleamento do jogo e a posição de onde o jogo será renderizado.
+ */
+
+class game_scaler{
+        /**
+     * @constructs
+     * Esse construtor define as opções de escaleamento e posicionamento do jogo
+     */
+    constructor(){
+        this.mode = Phaser.Scale.FIT;
+        this.autocenter = Phaser.Scale.CENTER_BOTH;
+    }
+
+    /**
+     * @description Essa Função cria apartir dos atributos da classe um objeto contendo os dados de escala para ser passado para o inicializador do Phaser.js
+     * @returns Retorna um objeto contendo os atributos da classe
+     */
+    make_scale_object(){
+        return {mode:this.mode,autoCenter:this.autocenter}
+    }
+}
+
+/**
+ * @author Gustavo Henrique S. S. Miranda
+ * @class 
+ * @classdesc Essa classe contem as configurações de fisica para o jogo.
+ */
+
+class game_physics{
+        /**
+     * @constructs
+     * Esse construtor define a configuração básica de fisica para o jogo.
+     */
+    constructor(){
+        this.default = 'arcade';
+        this.physicsDebug = true;
+    }
+    /**
+     * @description Essa Função cria apartir dos atributos da classe um objeto contendo as configuraçõe de fisica para ser passado para o inicializador do Phaser.js
+     * @returns Retorna um objeto contendo os atributos da classe
+     */
+    make_physics_object(){
+        var returnobject = {}
+        returnobject["default"] = this.default;
+        returnobject[this.default] = {};
+        returnobject[this.default]["debug"] = this.physicsDebug;
+
+        return returnobject;
+
+    }
+}
+
+
+/**
+ * @author Gustavo Henrique S. S. Miranda
+ * @class 
  * @classdesc Essa classe inicializa as configurações básicas necessárias para poder iniciar o funcionamento do 
  * Phaser.js  
  */
@@ -23,6 +79,8 @@ class game_config{
         this.width = width;
         this.height = height;
         this.game = null;
+        this.physics = new game_physics();
+        this.scale = new game_scaler();
 
     }
     /**
@@ -33,14 +91,17 @@ class game_config{
     }
     /**
      * @description Essa Função cria apartir dos atributos da classe um objeto para ser passado para o inicializador do Phaser.js
-     * @returns Retorna um objeto contendo os atributos da classe e as funções necessárias para o Phaser.js
+     * @returns Retorna um objeto contendo os atributos da classe necessárias para o Phaser.js
      */
     make_config_object(){
+        console.log(this.physics.make_physics_object());
         return {
             type:this.type,
             width:this.width,
             height:this.height,
-            scene:[main_game_scene]
+            scene:[main_game_scene],
+            physics:this.physics.make_physics_object(),
+            scale:this.scale.make_scale_object()
             
         }
     }
